21bc1d6844116bce892f8dd65efd0d4020bab58d
abbkrish <abbkrish@gmail.com>
2017-03-31 20:36:18 -0400

    Added about and signup

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 
 app = Flask(__name__)
@@ -7,6 +7,15 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+
+@app.route("/about")
+def about():
+	return redirect("http://www.orangesfoodpantry.org/about_us")
+
+
+@app.route("/signup")
+def signup():
+	return render_template("signup.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
